f87aaaba74802b95b92690e72f6eea9220999c3f
xunmeng2002 <150315803@qq.com>
2025-04-25 11:47:56 +0800

    Update TestMdApi.cpp

--- Test/TestMdApi/Src/TestMdApi.cpp
+++ Test/TestMdApi/Src/TestMdApi.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
 	api->RegisterFront(serverConfig.MdOfferAddress.c_str());
 	api->Init();
 
-	std::this_thread::sleep_for(chrono::seconds(10));
+	std::this_thread::sleep_for(chrono::seconds(5));
 	api->Release();
 	std::this_thread::sleep_for(chrono::seconds(1));
 
